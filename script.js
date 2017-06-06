c601577a6272c4de57c7d10e77abcc42c14af318
nicholas anggiansah <Mark@Hadys-MacBook-Pro.local>
2017-06-06 11:17:15 -0700

    added more styling

--- script.js
+++ script.js
@@ -16,8 +16,9 @@ function randomArrowsIndex(){
 // display arrow function
 function displayArrows(){
   gameStarted = true
+  buttonListener()
   var comboString = ""
-  for (i=0; i < 20; i++) {
+  for (i=0; i < 4; i++) {
     var arrowsIndex = randomArrowsIndex()
     comboArray.push(arrows[arrowsIndex].keycode)
     comboString += arrows[arrowsIndex].text + " "
@@ -27,74 +28,89 @@ function displayArrows(){
 }
 
 //player 1 event listener to input arrow keys
-$("body").on("keydown",function(event){
-  if (gameStarted) {
-    if (event.keyCode === 38) {
-      var monitor1Text = $('.monitor1').text()
-      $('.monitor1').text(monitor1Text + '↑')
-      player1Array.push(38)
+function buttonListener () {
+    $("body").on("keydown",function(event){
+    if (gameStarted) {
+      if (event.keyCode === 38) {
+        var monitor1Text = $('.monitor1').text()
+        $('.monitor1').text(monitor1Text + '↑')
+        player1Array.push(38)
+      }
+      if (event.keyCode === 40) {
+        var monitor1Text = $('.monitor1').text()
+        $('.monitor1').text(monitor1Text + '↓')
+        player1Array.push(40)
+      }
+      if (event.keyCode === 37) {
+        var monitor1Text = $('.monitor1').text()
+        $('.monitor1').text(monitor1Text + '←')
+        player1Array.push(37)
+      }
+      if (event.keyCode === 39) {
+        var monitor1Text = $('.monitor1').text()
+        $('.monitor1').text(monitor1Text + '→')
+        player1Array.push(39)
+      }
+      if (event.keyCode === 87) {
+        var monitor2Text = $('.monitor2').text()
+        $('.monitor2').text(monitor2Text + '↑')
+        player2Array.push(38)
+      }
+      if (event.keyCode === 83) {
+        var monitor2Text = $('.monitor2').text()
+        $('.monitor2').text(monitor2Text + '↓')
+        player2Array.push(40)
+      }
+      if (event.keyCode === 65) {
+        var monitor2Text = $('.monitor2').text()
+        $('.monitor2').text(monitor2Text + '←')
+        player2Array.push(37)
+      }
+      if (event.keyCode === 68) {
+        var monitor2Text = $('.monitor2').text()
+        $('.monitor2').text(monitor2Text + '→')
+        player2Array.push(39)
+      }
+      checkRoundWinner();
+      checkGameWinner();
     }
-    if (event.keyCode === 40) {
-      var monitor1Text = $('.monitor1').text()
-      $('.monitor1').text(monitor1Text + '↓')
-      player1Array.push(40)
-    }
-    if (event.keyCode === 37) {
-      var monitor1Text = $('.monitor1').text()
-      $('.monitor1').text(monitor1Text + '←')
-      player1Array.push(37)
-    }
-    if (event.keyCode === 39) {
-      var monitor1Text = $('.monitor1').text()
-      $('.monitor1').text(monitor1Text + '→')
-      player1Array.push(39)
-    }
-    if (event.keyCode === 87) {
-      var monitor2Text = $('.monitor2').text()
-      $('.monitor2').text(monitor2Text + '↑')
-      player2Array.push(38)
-    }
-    if (event.keyCode === 83) {
-      var monitor2Text = $('.monitor2').text()
-      $('.monitor2').text(monitor2Text + '↓')
-      player2Array.push(40)
-    }
-    if (event.keyCode === 65) {
-      var monitor2Text = $('.monitor2').text()
-      $('.monitor2').text(monitor2Text + '←')
-      player2Array.push(37)
-    }
-    if (event.keyCode === 68) {
-      var monitor2Text = $('.monitor2').text()
-      $('.monitor2').text(monitor2Text + '→')
-      player2Array.push(39)
-    }
-    checkWinner();
-  }
-})
+  })
+}
 
-function checkWinner () {
+function checkRoundWinner () {
   if (player1Array.join() === comboArray.join()) {
-    alert("Player 1 Wins! Player 2 loses 1 life")
+    alert("Player 1 wins the round! Player 2 loses 1 life")
     $('.player2lives').text($('.player2lives').text()-1)
     clearData()
     $('.playButton').fadeIn()
+    $("body").off("keydown")
   }
   else if (player2Array.join() === comboArray.join()) {
-    alert("Player 2 Wins! Player 1 loses 1 life")
+    alert("Player 2 wins the round! Player 1 loses 1 life")
     $('.player1lives').text($('.player1lives').text()-1)
     clearData()
     $('.playButton').fadeIn()
+    $("body").off("keydown")
   }
 }
 
-var clearData = function(){
+function clearData(){
   comboArray = [];
   player1Array = [];
   player2Array = [];
   $('.monitor1').text('');
   $('.monitor2').text('');
 }
+
+function checkGameWinner () {
+  if ($('.player1lives') = 0) {
+    alert("Player 2 wins the game!")
+  }
+  else if ($('.player2lives') = 0) {
+    alert("Player 1 wins the game!")
+  }
+}
+
 
 // var turn = 0
 // on keypress or keydown or keyup:
